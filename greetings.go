909e0174b31e23824e78c7123a4715c512d6a883
Fidelis Chimombe <fidelis.chimombe@ABSORBCHESTSOIL.workdayinternal.com>
2021-07-21 01:25:41 -0400

    added error handling to the greetings

--- greetings.go
+++ greetings.go
@@ -1,10 +1,17 @@
 package greetings
 
-import "fmt"
+import (
+    "fmt"
+    "errors"
+)
+
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
     // Return a greeting that embeds the name in a message.
+    if name == "" {
+        return "", errors.New("empty name")
+    }
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
     return message
 }
